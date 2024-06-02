b3302988b9fb3a6b61dd09955158e8c76284c496
kateshiAIV <148715019+kateshiAIV@users.noreply.github.com>
2024-06-02 19:40:52 +0200

    Добавьте файлы проекта.

---
+++ src/App.cpp
@@ -0,0 +1,46 @@
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1920,1080, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(1, 0, 0); // red
+        glVertex2f(-1.0f, -1.0f); //a
+        glColor3f(0, 1, 0); // green
+        glVertex2f(0.0f, 1.0f); //b
+        glColor3f(0, 0,1); // blue
+        glVertex2f(1.0f, -1.0f); //c
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
